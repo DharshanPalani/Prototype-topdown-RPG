56916d19222236bc95e223830144d72796bac3c7
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-06-04 21:11:32 +0530

    Animated player and implimented logic for that

--- Assets/Scripts/PlayerSystem/PlayerMovement.cs
+++ Assets/Scripts/PlayerSystem/PlayerMovement.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Player {
-
+namespace Player
+{
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private float _speed;
 
         private Animator _animator;
+        private SpriteRenderer _spriteRenderer;
+        private Rigidbody2D _rb2d;
 
         private Vector2 _movement;
-
-        private Rigidbody2D _rb2d;
 
         private void Start()
         {
@@ -24,20 +24,47 @@ namespace Player {
         {
             _movement.x = Input.GetAxisRaw("Horizontal");
             _movement.y = Input.GetAxisRaw("Vertical");
-
         }
 
         private void FixedUpdate()
         {
-            _rb2d.velocity = new Vector2(_movement.x * _speed, _movement.y * _speed);
+            _rb2d.velocity = _movement * _speed;
+
+            Vector2 velocity = _rb2d.velocity;
+
+            bool isMoving = velocity.magnitude > 0.1f;
+            _animator.SetBool("isMoving", isMoving);
+
+            if (isMoving)
+            {
+                // Use raw input for blend tree values, clamped between -1 and 1
+                Vector2 inputDir = _movement.normalized;
+
+                _animator.SetFloat("moveX", inputDir.x);
+                _animator.SetFloat("moveY", inputDir.y);
+
+                // Flip sprite based on horizontal input (not velocity)
+                if (_movement.x > 0.1f)
+                {
+                    _spriteRenderer.flipX = true;
+                }
+                else if (_movement.x < -0.1f)
+                {
+                    _spriteRenderer.flipX = false;
+                }
+            }
+
         }
 
         private void GetComponents()
         {
             _rb2d = GetComponent<Rigidbody2D>();
             _animator = GetComponent<Animator>();
-            if (_rb2d == null) Debug.Log("RigidBody is empty");
-            if (_animator == null) Debug.Log("Animator is empty");
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+
+            if (_rb2d == null) Debug.LogError("RigidBody is empty");
+            if (_animator == null) Debug.LogError("Animator is empty");
+            if (_spriteRenderer == null) Debug.LogError("SpriteRenderer is empty");
         }
     }
 }
